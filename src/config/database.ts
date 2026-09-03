2e014b9e3a531135fec721c55cfa7018862c7e3e
Hasnain Ali <sanwar990@example.com>
2026-09-03 15:09:33 +0500

    Add settings for Postman and update database connection URL in config

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,8 +3,7 @@ import { config } from './environment.js';
 
 export const connectDB = async (): Promise<void> => {
   try {
-    // await mongoose.connect(config.mongoose.url)
-    await mongoose.connect("mongodb://localhost:27017/library_management_system")
+    await mongoose.connect(config.mongoose.url))
 
   } catch (error) {
     console.error(`Error: ${(error as Error).message}`);
