f61f59f03a406db153c6d51a4f21fdb42b00e203
Hasnain Ali <sanwar990@example.com>
2026-09-14 22:34:09 +0500

    Refactor import statements for consistency and readability in borrowRoutes.ts

--- src/routes/borrowRoutes.ts
+++ src/routes/borrowRoutes.ts
@@ -1,14 +1,21 @@
-import express from 'express';
-import { issueBook, returnBook, getUserHistory, getAllBorrows, payFine, userBorrowBook } from '../controllers/borrowController.js';
-import { authenticate } from '../middleware/authenticate.js';
-import { authorize } from '../middleware/authorize.js';
+import express from "express";
+import {
+  issueBook,
+  returnBook,
+  getUserHistory,
+  getAllBorrows,
+  payFine,
+  userBorrowBook,
+} from "../controllers/borrowController.js";
+import { authenticate } from "../middleware/authenticate.js";
+import { authorize } from "../middleware/authorize.js";
 const router = express.Router();
 
-router.post('/issue', authenticate, authorize('admin'), issueBook);
-router.post('/borrow', authenticate, userBorrowBook);
-router.post('/return/:id', authenticate, authorize('admin'), returnBook);
-router.get('/history', authenticate, getUserHistory);
-router.get('/admin/all', authenticate, authorize('admin'), getAllBorrows);
-router.put('/pay-fine/:id', authenticate, authorize('admin'), payFine);
+router.post("/issue", authenticate, authorize("admin"), issueBook);
+router.post("/borrow", authenticate, userBorrowBook);
+router.post("/return/:id", authenticate, returnBook);
+router.get("/history", authenticate, getUserHistory);
+router.get("/admin/all", authenticate, authorize("admin"), getAllBorrows);
+router.put("/pay-fine/:id", authenticate, authorize("admin"), payFine);
 
 export default router;
